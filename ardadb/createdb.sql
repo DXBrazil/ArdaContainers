1b609c572f8ff16b7fbb28108abd2726250b86e4
Fabricio Catae <fcatae@microsoft.com>
2017-02-07 00:05:46 -0200

    create database

---
+++ ardadb/createdb.sql
@@ -0,0 +1,2 @@
+create database arda_kanban on (name='kanban_data', filename='c:\db\kanban.mdf') log on (name='kanban_log', filename='c:\db\kanban.ldf')
+create database arda_permissions on (name='permissions_data', filename='c:\db\permissions.mdf') log on (name='permissions_log', filename='c:\db\permissions.ldf')
